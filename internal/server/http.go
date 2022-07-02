318102954457f79d0003869406e847fa5e24a7f0
Thiago Melo <thmelo@fb.com>
2022-07-02 14:07:12 +0100

    first commit

---
+++ internal/server/http.go
@@ -0,0 +1,99 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// NewHTTPServer creates a Log server
+func NewHTTPServer(addr string) *http.Server {
+	httpsvr := newHTTPServer()
+	r := mux.NewRouter()
+	r.HandleFunc("/", httpsvr.handleProduce).Methods("POST")
+	r.HandleFunc("/", httpsvr.handleConsume).Methods("GET")
+	return &http.Server{
+		Addr:         addr,
+		Handler:      r,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 60 * time.Second,
+	}
+}
+
+type httpServer struct {
+	Log *Log
+}
+
+func newHTTPServer() *httpServer {
+	return &httpServer{
+		Log: NewLog(),
+	}
+}
+
+func (s httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+	// unmarshal request
+	var req ProduceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// run the logic
+	off, err := s.Log.Append(req.Record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// marshal response
+	res := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+	// unmarshal request
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// run the logic
+	record, err := s.Log.Read(req.Offset)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// marshal response
+	res := ConsumeResponse{Record: record}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// ProduceRequest encapsulates a produce request
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+// ProduceResponse encapsulates a produce response
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+// ConsumeRequest encapsulates a consume request
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+// ConsumeResponse encapsulates a consume response
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
---
+++ internal/server/log.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Log is an in memory log
+type Log struct {
+	mu      sync.Mutex
+	records []Record
+}
+
+// NewLog produces a new Log
+func NewLog() *Log {
+	return &Log{}
+}
+
+// Append adds a new entry to the in memory log
+func (c *Log) Append(record Record) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	record.Offset = uint64(len(c.records))
+	c.records = append(c.records, record)
+	return record.Offset, nil
+}
+
+// Read return a record from the in memory log by its offset
+func (c *Log) Read(offset uint64) (Record, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if offset >= uint64(len(c.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+	return c.records[offset], nil
+}
+
+// ErrOffsetNotFound informs that a record with such offset cannot be found
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
+
+// Record represents a log entry
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
